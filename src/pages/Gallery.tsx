b40e41f73d21d0ad725e97b15554bee9e4ff49ad
iv_androo <evandroa174@gmail.com>
2026-09-19 23:50:32 +0100

    retificando o espaço do titulo da galeria

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -4,7 +4,7 @@ export default function Gallery() {
   return (
     <section>
       <div className="bg-studio-3 flex flex-col ">
-        <div className="flex flex-col w-full max-w-6xl px-8 py-14 my-0 mx-auto gap-4 sm:px-0">
+        <div className="flex flex-col w-full max-w-6xl px-8 py-14 my-0 mx-auto gap-4">
           <h1 className="text-5xl text-paper-dim font-anto">Galeria</h1>
           <p className="text-paper max-w-[60ch]">
             Momentos das nossas formações — fotos e vídeos das turmas, dos
